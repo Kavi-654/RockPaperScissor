96e9289ed0441f84bdc8ceafcb758b808e35880d
Kavinayasri <146553129+Kavi-654@users.noreply.github.com>
2026-03-12 14:10:07 +0530

    Create Color.kt

---
+++ ui/theme/Color.kt
@@ -0,0 +1,41 @@
+package com.example.rockpaperscissor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PurpleSurfaceLight = Color(0xFFE4D9F6)
+val OnPurpleSurfaceLight = Color(0xFFCB82D0)
+
+val PurpleSurfaceVariantLight = Color(0xFFE7E0EC)
+val OnPurpleSurfaceVariantLight = Color(0xFF49454F)
+
+val PurpleSurfaceDark = Color(0xFF6B648D)
+val OnPurpleSurfaceDark = Color(0xFFAD56BD)
+
+val PurpleSurfaceVariantDark = Color(0xFF4A3A5A)
+val OnPurpleSurfaceVariantDark = Color(0xFFE6E0EC)
+
+val PurplePrimaryLight = Color(0xFF6750A4)        // main purple
+val OnPurplePrimaryLight = Color(0xFFFFFFFF)
+
+val PurpleSecondaryLight = Color(0xFF625B71)      // muted purple-gray
+val OnPurpleSecondaryLight = Color(0xFFFFFFFF)
+
+val PurpleTertiaryLight = Color(0xFF7D5260)       // soft accent plum
+val OnPurpleTertiaryLight = Color(0xFFFFFFFF)
+
+val PurplePrimaryDark = Color(0xFFD0BCFF)
+val OnPurplePrimaryDark = Color(0xFF381E72)
+
+val PurpleSecondaryDark = Color(0xFFCCC2DC)
+val OnPurpleSecondaryDark = Color(0xFF332D41)
+
+val PurpleTertiaryDark = Color(0xFFEFB8C8)
+val OnPurpleTertiaryDark = Color(0xFF492532)
